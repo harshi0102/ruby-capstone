1bc52b95896f8ba910b86a773ce2fabec90b36cf
unknown <harshika0102@gmail.com>
2023-07-13 21:46:01 +0530

    Implement methods Game and Author class

--- game-and-author/author.rb
+++ game-and-author/author.rb
@@ -1,15 +1,17 @@
-require_relative '../app/item'
+class Author
+  attr_accessor :id, :first_name, :last_name
+  attr_reader :items
 
-class Author < Item
-  attr_accessor :id, :first_name, :last_name, :items
-
-  def initialize(id, first_name, last_name, items)
+  def initialize(first_name:, last_name:, id: rand(1000))
     super
     @id = id
     @first_name = first_name
     @last_name = last_name
-    @items = items
+    @items = []
   end
 
-  def add_item(item)end
+  def add_item(item)
+    @items << item
+    item.add_author self
+  end
 end
