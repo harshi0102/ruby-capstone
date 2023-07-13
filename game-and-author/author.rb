0e55688a64e300fdd9a100eb1b07a56a47ff8ecc
unknown <harshika0102@gmail.com>
2023-07-13 23:35:02 +0530

    Add methods for adding and displaying books and labels

--- game-and-author/author.rb
+++ game-and-author/author.rb
@@ -1,17 +1,15 @@
-class Author
-  attr_accessor :id, :first_name, :last_name
-  attr_reader :items
+require_relative '../app/item'
 
-  def initialize(first_name:, last_name:, id: rand(1000))
+class Author < Item
+  attr_accessor :id, :first_name, :last_name, :items
+
+  def initialize(id, first_name, last_name, items)
     super
     @id = id
     @first_name = first_name
     @last_name = last_name
-    @items = []
+    @items = items
   end
 
-  def add_item(item)
-    @items << item
-    item.add_author self
-  end
+  def add_item(item)end
 end
